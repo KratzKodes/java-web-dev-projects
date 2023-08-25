fcfbedc5e2cff263c58b7cc6df02e6df5c92bd4a
Maurice Kratz <kratzkodes@gmail.com>
2023-08-24 21:28:38 -0400

    cleaned code

--- control-flow-and-collections/CountingCharacters.java
+++ control-flow-and-collections/CountingCharacters.java
@@ -3,8 +3,6 @@ import java.util.Arrays;
 import java.util.Scanner;
 
 public interface CountingCharacters {
-<<<<<<< HEAD
-=======
 
     String hiddenFiguresQuote = "If the product of two terms is zero then common sense says " +
             "at least one of the two terms has to be zero to start with. So if you move all " +
@@ -13,34 +11,10 @@ public interface CountingCharacters {
             "it’s pretty straightforward from there.";
 
     char[] hiddenFiguresQuoteCharArray = hiddenFiguresQuote.toLowerCase().toCharArray();
->>>>>>> origin/main
     public static void main(String[] args) {
 
         Scanner input = new Scanner(System.in);
 
-<<<<<<< HEAD
-        //Gather User Input
-        System.out.println("Enter a phrase");
-
-        //Convert user input to lowercase and store as an array
-        String userStatement = input.nextLine().toLowerCase();
-        char[] userStatementCharArray = userStatement.toCharArray();
-
-        //Initialize a hashmap to store each character and its value
-        HashMap<String,Integer> charCount = new HashMap<>();
-
-        //Loop through each character in the array
-        for (char character : userStatementCharArray) {
-            //Determine if the character is alphabetic
-            if (Character.isAlphabetic(character)) {
-                //Convert the char to a String
-                String quoteCharacter = Character.toString(character);
-                //Check to see if the hashmap contains that key and update appropriately
-                if (charCount.containsKey(quoteCharacter)) {
-                    charCount.put(quoteCharacter, charCount.get(quoteCharacter) + 1);
-                } else {
-                    charCount.put(Character.toString(character), 1);
-=======
         System.out.println("Enter a phrase");
         String userStatement = input.nextLine().toLowerCase();
         char[] userStatementCharArray = userStatement.toCharArray();
@@ -55,7 +29,6 @@ public interface CountingCharacters {
                     charCount.put(quoteCharacter,charCount.get(quoteCharacter)+1);
                 }else{
                     charCount.put(Character.toString(userStatementCharArray[i]),1);
->>>>>>> origin/main
                 }
             }
         }
