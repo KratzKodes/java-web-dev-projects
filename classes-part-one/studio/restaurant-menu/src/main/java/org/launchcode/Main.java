28042cccdb0feced162714d73d351382c73cdcde
Maurice Kratz <kratzkodes@gmail.com>
2023-08-28 21:11:33 -0400

    added menu items to restaurant menu

--- classes-part-one/studio/restaurant-menu/src/main/java/org/launchcode/Main.java
+++ classes-part-one/studio/restaurant-menu/src/main/java/org/launchcode/Main.java
@@ -1,8 +1,27 @@
 package org.launchcode;
 
+
 public class Main {
 
     public static void main(String[] args) {
 	// write your code here
+        Menu menu = new Menu();
+
+        MenuItem pizza = new MenuItem("Pizza", "Cheese bread","Dinner", 15.99);
+        MenuItem chickenPasta = new MenuItem("Chicken Pasta", "Fettuccine with grilled free roam chicken ","Dinner", 18.99);
+        MenuItem iceCream = new MenuItem("Ice Cream", "Vanilla butter churned","Dessert", 5.99);
+        MenuItem water = new MenuItem("Water", "Spring Water","Beverage");
+        MenuItem wine = new MenuItem("Wine", "Italian wine","Alcohol", 19.99);
+
+        menu.addMenuItem(pizza);
+        menu.addMenuItem(chickenPasta);
+        menu.addMenuItem(iceCream);
+        menu.addMenuItem(water);
+        menu.addMenuItem(wine);
+
+        menu.printMenu();
+
     }
+
+
 }
